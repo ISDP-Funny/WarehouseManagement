95653fa70feb0ebc85328352aa0e93b0dbb0af9f
Adikso <adam@zambrzycki.net>
2020-02-05 15:11:54 +0100

    Improve tables creation error handling

--- scripts/prepare_database.sh
+++ scripts/prepare_database.sh
@@ -3,8 +3,18 @@ export DERBY_OPTS='-Dij.database="jdbc:derby://localhost:1527/WM;create=true;use
 
 if ij scripts/test_tables.sql | grep -q "not exist"; then
   echo "Tables not found. Seeding database"
-  echo $(ij src/main/resources/createDB.sql)
-  echo $(ij src/main/resources/initDB.sql)
+
+  CREATE_DB="$(ij src/main/resources/createDB.sql)"
+  if echo "$CREATE_DB" | grep -q "ERROR"; then
+    echo "Failed to create tables"
+    exit 2
+  fi
+
+  INIT_DB="$(ij src/main/resources/initDB.sql)"
+  if echo "$INIT_DB" | grep -q "ERROR"; then
+    echo "Failed to seed tables"
+    exit 3
+  fi
 
   if ij scripts/test_tables.sql | grep -q "selected"; then
     exit $?
